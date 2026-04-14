92675fef281eede33c5408787a1e69d94f4f0700
AlexRiveraExc <20243ds065@utez.edu.mx>
2026-04-14 14:26:38 -0600

    se conectan botones de formulario y detalle

--- src/MainController.java
+++ src/MainController.java
@@ -2,6 +2,11 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.control.Alert;
 
 public class MainController {
 
@@ -90,3 +95,67 @@ public void exportar() {
     alert.setContentText("Reporte exportado");
     alert.show();
 }
+
+@FXML
+public void nuevo() {
+
+    try {
+
+        FXMLLoader loader =
+                new FXMLLoader(
+                        getClass().getResource("/formulario-view.fxml")
+                );
+
+        Parent root = loader.load();
+
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.setTitle("Nuevo Libro");
+        stage.show();
+
+    } catch (Exception e) {
+
+        System.out.println("Error abriendo formulario");
+    }
+}
+
+@FXML
+public void verDetalle() {
+
+    Libro libro =
+            tablaLibros.getSelectionModel()
+                    .getSelectedItem();
+
+    if (libro == null) {
+
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setContentText("Selecciona un libro");
+        alert.show();
+        return;
+    }
+
+    try {
+
+        FXMLLoader loader =
+                new FXMLLoader(
+                        getClass().getResource("/detalle-view.fxml")
+                );
+
+        Parent root = loader.load();
+
+        DetalleController controller =
+                loader.getController();
+
+        controller.setLibro(libro);
+
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.setTitle("Detalle Libro");
+        stage.show();
+
+    } catch (Exception e) {
+
+        System.out.println("Error abriendo detalle");
+    }
+}
+
